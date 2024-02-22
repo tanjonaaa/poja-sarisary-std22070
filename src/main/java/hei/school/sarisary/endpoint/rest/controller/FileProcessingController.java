b6927d6a922b194efdc41dd7b664e5389560e9e5
tanjonaaa <hei.tanjona@gmail.com>
2024-02-22 10:36:37 +0100

    feat: grayscale and upload to s3

--- src/main/java/hei/school/sarisary/endpoint/rest/controller/FileProcessingController.java
+++ src/main/java/hei/school/sarisary/endpoint/rest/controller/FileProcessingController.java
@@ -3,11 +3,12 @@ package hei.school.sarisary.endpoint.rest.controller;
 import hei.school.sarisary.service.FileProcessingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class FileProcessingController {
@@ -19,8 +20,15 @@ public class FileProcessingController {
     this.fileProcessingService = fileProcessingService;
   }
 
-  @PutMapping("/black-and-white/{id}")
-  public ResponseEntity<String> putBlackAndWhite(@PathVariable String id) {
+  @RequestMapping(
+          path = "/black-and-white/{id}",
+          method = RequestMethod.PUT,
+          consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}
+  )
+  public ResponseEntity<String> putBlackAndWhite(
+          @PathVariable String id,
+          @RequestPart("file") MultipartFile file) throws IOException {
+    this.fileProcessingService.process(id, file);
     return ResponseEntity.status(HttpStatus.OK).body("OK");
   }
 
--- src/main/java/hei/school/sarisary/service/FileProcessingService.java
+++ src/main/java/hei/school/sarisary/service/FileProcessingService.java
@@ -2,18 +2,62 @@ package hei.school.sarisary.service;
 
 import hei.school.sarisary.file.BucketComponent;
 import hei.school.sarisary.repository.FileProcessingRepository;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.process.ImageConverter;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
 
 @Service
 public class FileProcessingService {
   private final BucketComponent bucketComponent;
   private final FileProcessingRepository fileProcessingRepository;
+  private final String DIRECTORY = "sary/";
 
   @Autowired
   public FileProcessingService(
       BucketComponent bucketComponent, FileProcessingRepository fileProcessingRepository) {
     this.bucketComponent = bucketComponent;
     this.fileProcessingRepository = fileProcessingRepository;
+  }
+
+  private String getExtension(String filename){
+    return FilenameUtils.getExtension(filename);
+  }
+
+  public void process(String id, MultipartFile file) throws IOException {
+    String extension = this.getExtension(file.getName());
+    File initial = File.createTempFile(
+            id + "initial",
+            "." + extension
+    );
+    file.transferTo(initial);
+    File transformed = this.toBlackAndWhite(initial);
+
+    this.uploadToS3(id + "-initial", initial);
+    this.uploadToS3(id + "-black-and-white", transformed);
+  }
+
+  private File toBlackAndWhite(File file) throws IOException {
+    String extension = this.getExtension(file.getName());
+    ImagePlus originalImage = IJ.openImage(file.getAbsolutePath());
+    new ImageConverter(originalImage).convertToGray32();
+    File transformed = File.createTempFile("transformed", "." + extension);
+    ImageIO.write(originalImage.getBufferedImage(), extension, transformed);
+    return transformed;
+  }
+
+  private void uploadToS3(String prefix, File toUpload) throws IOException {
+    String suffix = "." + FilenameUtils.getExtension(toUpload.getName());
+    String bucketKey = DIRECTORY + prefix + suffix;
+
+    bucketComponent.upload(toUpload, bucketKey);
   }
 }
